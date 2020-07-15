14cbec8b4c355fdb25cb0960034ebf22ae1d4fe0
jberrio <jberrio@transfiriendo.com>
2020-07-14 21:20:25 -0500

    Renombrando carpeta build por docs

---
+++ docs/precache-manifest.58c0670d64640c47fe59eef7748e6bb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ccc79770fe6c43ff4c22255d55d9bc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fe2180caabde054417e",
+    "url": "/static/css/main.b89ec05e.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fe2180caabde054417e",
+    "url": "/static/js/main.5984ddd9.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
